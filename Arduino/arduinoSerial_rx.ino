ee1bc0f5250f266758284e68488225bc5b18b0c4
JoseMJaramilloS <josem.jaramillo@udea.edu.co>
2022-03-02 10:54:56 -0500

    Se corrigen los codigos de Arduino para la comunicacion serial

--- Arduino/arduinoSerial_rx.ino
+++ Arduino/arduinoSerial_rx.ino
@@ -1,34 +1,19 @@
 void setup()
 {
-  for(int i=4;i<8;i++){
-  	pinMode(i, OUTPUT);
-  }  
+  for(int i=2;i<4;i++){
+  	pinMode(i, INPUT);
+  } 
+  Serial.begin(9600);
 }
-int binario[24]={0,1,0,1,1,1,1,1,0,1,0,0,0,1,0,1,0,1,1,1,1,1,1,1};
-int tiempo=500;//variable tiempo que define el periodo del reloj
+bool dato=LOW,datoAnterior=LOW;
 void loop()
 {
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(4, HIGH);
-  
-  
-  for(int i=0;i<24;i++){//debe recorrer todo el arreglo
-    digitalWrite(5, LOW);
-    if(binario[i]==1) digitalWrite(6, HIGH);
-    else digitalWrite(6, LOW);
-    delay(tiempo/20);//50
-    digitalWrite(7, HIGH);
-    if((i+1)%8==0) digitalWrite(5, HIGH);//debe ser multiplo de la particion
-    
-    delay(0.5); //un tiempo muy bajo de reloj, garantiza que solo se tome un dato en el otro arduino
-    digitalWrite(7, LOW);
-    delay(tiempo-tiempo/2-tiempo/20-0.5);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    delay(tiempo/2); 
-    
-    
+  dato=digitalRead(2);
+  if(dato==HIGH && datoAnterior==LOW){ //detector de flancos
+    //Serial.println("reloj");
+  	if(digitalRead(3)==HIGH) Serial.println(1);
+    else if(digitalRead(3)==LOW) Serial.println(0);
   }
-  
+  datoAnterior==dato;
+  //if(digitalRead(3)==HIGH) Serial.println(1);
 }
--- Arduino/arduinoSerial_tx.ino
+++ Arduino/arduinoSerial_tx.ino
@@ -1,19 +1,34 @@
 void setup()
 {
-  for(int i=2;i<4;i++){
-  	pinMode(i, INPUT);
-  } 
-  Serial.begin(9600);
+  for(int i=4;i<8;i++){
+  	pinMode(i, OUTPUT);
+  }  
 }
-bool dato=LOW,datoAnterior=LOW;
+int binario[24]={0,1,0,1,1,1,1,1,0,1,0,0,0,1,0,1,0,1,1,1,1,1,1,1};
+int tiempo=500;//variable tiempo que define el periodo del reloj
 void loop()
 {
-  dato=digitalRead(2);
-  if(dato==HIGH && datoAnterior==LOW){ //detector de flancos
-    //Serial.println("reloj");
-  	if(digitalRead(3)==HIGH) Serial.println(1);
-    else if(digitalRead(3)==LOW) Serial.println(0);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(4, HIGH);
+  
+  
+  for(int i=0;i<24;i++){//debe recorrer todo el arreglo
+    digitalWrite(5, LOW);
+    if(binario[i]==1) digitalWrite(6, HIGH);
+    else digitalWrite(6, LOW);
+    delay(tiempo/20);//50
+    digitalWrite(7, HIGH);
+    if((i+1)%8==0) digitalWrite(5, HIGH);//debe ser multiplo de la particion
+    
+    delay(0.5); //un tiempo muy bajo de reloj, garantiza que solo se tome un dato en el otro arduino
+    digitalWrite(7, LOW);
+    delay(tiempo-tiempo/2-tiempo/20-0.5);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    delay(tiempo/2); 
+    
+    
   }
-  datoAnterior==dato;
-  //if(digitalRead(3)==HIGH) Serial.println(1);
+  
 }
